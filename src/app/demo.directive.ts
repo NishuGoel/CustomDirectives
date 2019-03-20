a4c6ab0e37d4c2951ad97e51b0348489ff309e21
NishuGoel <nishu0505@gmail.com>
2019-03-20 16:14:05 +0530

    Update demo.directive.ts

--- src/app/demo.directive.ts
+++ src/app/demo.directive.ts
@@ -5,16 +5,25 @@ import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@ang
 })
 export class DemoDirective {
 
+  
+  
+  //Injecting services to use the directives
   constructor(private el: ElementRef, private renderer: Renderer) {
    // this.changeColor('red');
 
   }
+  
+  
+  //using HostBinding() to capture the property of the Host Element
   @HostBinding('style.border') border: string;
+  
+  
+  //using HostListener() to capture the event of the Host Element
   @HostListener('click') onclick(){
     this.changeColor('blue');
     this.border = '20px solid green';
   }
    changeColor(color: string) {
-    this.renderer.setElementStyle(this.el.nativeElement, 'color', color);
+    this.renderer.setElementStyle(this.el.nativeElement, 'color', color); //applying the directive
   }
 }
